e98cc5ef1a274018d93688ceeb796ece0ddb5ac4
John Carl <johncarlsantos30@gmail.com>
2026-04-01 01:08:23 +0800

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Navbar from '@/components/Navbar';
-import SideNavbar from '../section/side-navbar';
 
 export default function AboutPage() {
   return (
@@ -91,8 +90,6 @@ export default function AboutPage() {
         </div>
 
       </div>
-
-      <SideNavbar />
     </div>
   );
 }
