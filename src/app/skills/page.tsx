1b9c6c2c494b9d59926ef9182a384fe3755c9274
John Carl <johncarlsantos30@gmail.com>
2026-08-11 06:21:33 +0800

    Update page.tsx

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-import { useEffect } from 'react';
-import HomeComponent from "@/components/Home";
+import SectionPage from "@/components/SectionPage";
 
 export default function SkillsPage() {
-  useEffect(() => {
-    const element = document.getElementById('skills');
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, []);
-
-  return <HomeComponent />;
+  return <SectionPage targetId="skills" />;
 }
