5486aa7eb0196b5040257a7cf95b7f88b99715dd
John Carl <johncarlsantos30@gmail.com>
2026-08-11 06:21:24 +0800

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-import { useEffect } from 'react';
-import HomeComponent from "@/components/Home";
+import SectionPage from "@/components/SectionPage";
 
 export default function ContactPage() {
-  useEffect(() => {
-    const element = document.getElementById('contact');
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, []);
-
-  return <HomeComponent />;
+  return <SectionPage targetId="contact" />;
 }
